e6b048e8f980277f0f8a043b30a147dc3a497b83
matt johannsen <majohann@hotmail.com>
2020-03-09 13:31:43 -0500

    Added ProblemTwo class

---
+++ LINQ_Problems/ProblemTwo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Problems
+{
+    class ProblemTwo
+    {
+        public List<string> names;
+
+        public ProblemTwo()
+        {
+            names = new List<string>() { "Mike", "Brad", "Nevin", "Ian", "Mike" };
+        }
+    }
+}
--- LINQ_Problems/Program.cs
+++ LINQ_Problems/Program.cs
@@ -10,15 +10,18 @@ namespace LINQ_Problems
     {
         static void Main(string[] args)
         {
-            //This is Problem One
-            Console.WriteLine("This is Problem One");
-            ProblemOne problemOne = new ProblemOne();
-            var wordsWithTH = problemOne.words.Where(m => m.Contains("th"));
-            foreach (var item in wordsWithTH)
-            {
-                Console.Write($"{item} ");
-            }
+            //Start of Problem One
+            //Console.WriteLine("This is Problem One");
+            //ProblemOne problemOne = new ProblemOne();
+            //var wordsWithTH = problemOne.words.Where(m => m.Contains("th"));
+            //foreach (var item in wordsWithTH)
+            //{
+            //    Console.Write($"{item} ");
+            //}
             //End of Problem One
+            // Start of Problem Two
+
+            // End of Problem Two
             Console.ReadLine();
         }
     }
