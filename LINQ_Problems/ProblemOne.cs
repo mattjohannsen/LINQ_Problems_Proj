1beedf0fa6cc669a150326282bada935b3a71d71
matt johannsen <majohann@hotmail.com>
2020-03-09 13:18:24 -0500

    First working example of Problem One

--- LINQ_Problems/ProblemOne.cs
+++ LINQ_Problems/ProblemOne.cs
@@ -8,7 +8,37 @@ namespace LINQ_Problems
 {
     public class ProblemOne
     {
+        public List<string> words;
 
+        public ProblemOne()
+        {
+            words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
+
+        }
+        //public GetList()
+        //{
+        //    words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
+
+        //    return words;
+        //}
+        
+        public bool CheckForTH(string input)
+        {
+            //bool a = list.Contains<int>(7);
+            //// Use instance method.
+            //bool b = list.Contains(7);
+
+            //Console.WriteLine(a);
+            //Console.WriteLine(b);
+            return false;
+        }
+        public void PrintList(List<string> inputList)
+        {
+            foreach (var input in inputList)
+            {
+                Console.Write($"{input} ");
+            }
+        }
         
     }
 }
--- LINQ_Problems/Program.cs
+++ LINQ_Problems/Program.cs
@@ -11,6 +11,38 @@ namespace LINQ_Problems
         static void Main(string[] args)
         {
             Console.WriteLine("This is Problem One");
+            //ProblemOne theProblemOne = new ProblemOne();
+            //theProblemOne.PrintList(theProblemOne.words);
+            //foreach (var item in theProblemOne.words)
+            //{
+            //    theProblemOne.CheckForTH(item);
+            //    Console.WriteLine(item);
+            //}
+            //theProblemOne.CheckForTH();
+            ProblemOne problemOne = new ProblemOne();
+            string stringToCheck = "th";
+            var wordlist = problemOne.words;
+            var wordsWithTH = wordlist.Where(m => m.Contains(stringToCheck));
+            //List<ProblemOne> words = problemOne.GetList();
+            //List<ProblemOne> wordsWithoutTh = words.Contains(m => m.CheckForTH = false);
+            foreach (var item in wordsWithTH)
+            {
+                Console.Write($"{item} ");
+            }
+
+
+
+            //MovieLibrary movieLibrary = new MovieLibrary();
+            //List<Movie> movies = movieLibrary.GetMovies();
+            ////List<Movie> hourAndAHalfMovieRunTime = movies.FindAll(m => m.runTime > 90);
+            //foreach (var movie in hourAndAHalfMovieRunTime)
+            //{
+            //    Console.WriteLine(movie.title);
+            //}
+
+            //var ninetyMinuteMovies = movies.Where(m => m.runTime >= 90);
+            //var ninentyMinuteMoviesSorted = ninetyMinuteMovies.OrderBy(m => m.title);
+
 
             Console.ReadLine();
         }
