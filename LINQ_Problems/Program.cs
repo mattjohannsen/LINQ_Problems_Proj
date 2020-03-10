810fa0869ab71269433ee871921375281314de6d
matt johannsen <majohann@hotmail.com>
2020-03-10 09:09:28 -0500

    Problem Three technically works, but only if I make the answer each time it loops through

--- LINQ_Problems/Program.cs
+++ LINQ_Problems/Program.cs
@@ -21,35 +21,35 @@ namespace LINQ_Problems
             //End of Problem One
 
             // Start of Problem Two
-            Console.WriteLine("This is Problem Two");
-            ProblemTwo problemTwo = new ProblemTwo();
-            //var noDuplicates = problemTwo.names.Distinct();
-            var noDuplicates = problemTwo.names.OrderBy(s => new string(s.ToCharArray())).Distinct();
-            foreach (var item in noDuplicates)
-            {
-                Console.Write($"{item} ");
-            }
+            //Console.WriteLine("This is Problem Two");
+            //ProblemTwo problemTwo = new ProblemTwo();
+            ////var noDuplicates = problemTwo.names.Distinct();
+            //var noDuplicates = problemTwo.names.OrderBy(s => new string(s.ToCharArray())).Distinct();
+            //foreach (var item in noDuplicates)
+            //{
+            //    Console.Write($"{item} ");
+            //}
             // End of Problem Two
 
             //Start of Problem Three
-            //Console.WriteLine("This is Problem Three");
-            //ProblemThree problemThree = new ProblemThree();
-            ////var turnStringToList = problemThree.classGrades.ToList();
-            //foreach (var item in problemThree.classGrades)
-            //{
-            //    var temp = new List<int>(item.Split(',').Select(s => int.Parse(s)));
-            //    foreach (var item2 in temp)
-            //    {
-            //        Console.WriteLine($"{item2}");
-            //    }
-            //    Console.WriteLine(".....");
-            //    //var listWithLowestRemoved = new List<int>(temp.Remove(temp.Min());
-            //    //foreach (var item3 in listWithLowestRemoved)
-            //    //{
-            //    //    Console.WriteLine($"{item}");
-            //    //}
+            Console.WriteLine("This is Problem Three");
+            ProblemThree problemThree = new ProblemThree();
+            foreach (var item in problemThree.classGrades)
+            {
+                //This is the error free code so far.
+                //var temp = new List<int>(item.Split(',').Select(s => int.Parse(s)).ToList().OrderByDescending(i => i).Take(problemThree.classGrades.Count));
+                //I am running into problems when I try to get an average.
+                //var temp = new List<int>(item.Split(',').Select(s => int.Parse(s)).ToList().OrderByDescending(i => i).Take(problemThree.classGrades.Count));
+                var average = problemThree.classGrades.Select(g => g.Split(',').Select(int.Parse).ToList().OrderByDescending(i => i).Take(problemThree.classGrades.Count).Average()).Average();
+                Console.WriteLine($"{average}");
+                //foreach (var item2 in average)
+                //{
+                //    Console.WriteLine($"{item2}");
+                //}
+                Console.WriteLine(".....");
 
-            //}
+            }
+
             //var turnStringToList = problemThree.classGrades.ToList();
             //turnStringToList.Select(int.Parse).ToList();
             //var turnListToInts = turnStringToList.Select(s => int.Parse(s));
