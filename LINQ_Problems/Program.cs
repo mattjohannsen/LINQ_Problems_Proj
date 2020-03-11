ebb5a1fe2f9b7b7c562f9f60da32a63641f4d449
matt johannsen <majohann@hotmail.com>
2020-03-11 08:35:49 -0500

    Problem Three works correctly without having to repeat the correct answer in loop

--- LINQ_Problems/Program.cs
+++ LINQ_Problems/Program.cs
@@ -32,29 +32,34 @@ namespace LINQ_Problems
             // End of Problem Two
 
             //Start of Problem Three
-            //Console.WriteLine("This is Problem Three");
-            //ProblemThree problemThree = new ProblemThree();
-            //foreach (var item in problemThree.classGrades)
-            //{
-            //    var average = problemThree.classGrades.Select(g => g.Split(',').Select(int.Parse).ToList().OrderByDescending(i => i).Take(problemThree.classGrades.Count).Average()).Average();
-            //    Console.WriteLine($"{average}");
-            //    Console.WriteLine(".....");
-            //}
+            Console.WriteLine("This is Problem Three");
+            ProblemThree problemThree = new ProblemThree();
+            double returnedAverage = 0;
+            foreach (var item in problemThree.classGrades)
+            {
+                var average = problemThree.classGrades.Select(g => g.Split(',').Select(int.Parse).ToList().OrderByDescending(i => i).Take(problemThree.classGrades.Count).Average()).Average();
+                //Console.WriteLine($"{average}");
+                //Console.WriteLine(".....");
+                returnedAverage = Convert.ToDouble(average);
+            }
+            Console.WriteLine($"{returnedAverage}");
             //End of Problem Three
 
             //Start of Problem Four
-            Console.WriteLine("This is Problem Four");
-            ProblemFour problemFour = new ProblemFour();
-            var stringToList = problemFour.inputString.ToList();
-            foreach (var item in stringToList)
-            {
-                Console.WriteLine($"{item}");
-            }
-            var stringListOrdered = problemFour.inputString.OrderBy(x => x).ToList();
-            foreach (var item in stringListOrdered)
-            {
-                Console.WriteLine($"{item}");
-            }
+            ////Console.WriteLine("This is Problem Four");
+            ////ProblemFour problemFour = new ProblemFour();
+            ////var stringListOrdered = problemFour.inputString.OrderBy(x => x).ToArray().Count();
+            ////Console.WriteLine($"{stringListOrdered}");
+            //var stringToList = problemFour.inputString.ToList();
+            //foreach (var item in stringToList)
+            //{
+            //    Console.WriteLine($"{item}");
+            //}
+
+            //foreach (var item in stringListOrdered)
+            //{
+            //    Console.WriteLine($"{item}");
+            //}
             //End of Problem Four
             Console.ReadLine();
         }
