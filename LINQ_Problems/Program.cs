c7e3cefbf6f71c6d11075b3151ab1d5db919d0b9
matt johannsen <majohann@hotmail.com>
2020-03-11 14:00:57 -0500

    Cleaned up display of Linq Problems

--- LINQ_Problems/Program.cs
+++ LINQ_Problems/Program.cs
@@ -11,45 +11,48 @@ namespace LINQ_Problems
         static void Main(string[] args)
         {
             //Start of Problem One
-            //Console.WriteLine("This is Problem One");
-            //ProblemOne problemOne = new ProblemOne();
-            //var wordsWithTH = problemOne.words.Where(m => m.Contains("th"));
-            //foreach (var item in wordsWithTH)
-            //{
-            //    Console.Write($"{item} ");
-            //}
+            Console.WriteLine();
+            Console.WriteLine("This is Problem One:");
+            ProblemOne problemOne = new ProblemOne();
+            var wordsWithTH = problemOne.words.Where(m => m.Contains("th"));
+            foreach (var item in wordsWithTH)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine();
             //End of Problem One
 
             // Start of Problem Two
-            //Console.WriteLine("This is Problem Two");
-            //ProblemTwo problemTwo = new ProblemTwo();
-            ////var noDuplicates = problemTwo.names.Distinct();
-            //var noDuplicates = problemTwo.names.OrderBy(s => new string(s.ToCharArray())).Distinct();
-            //foreach (var item in noDuplicates)
-            //{
-            //    Console.Write($"{item} ");
-            //}
+            Console.WriteLine();
+            Console.WriteLine("This is Problem Two:");
+            ProblemTwo problemTwo = new ProblemTwo();
+            //var noDuplicates = problemTwo.names.Distinct();
+            var noDuplicates = problemTwo.names.OrderBy(s => new string(s.ToCharArray())).Distinct();
+            foreach (var item in noDuplicates)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine();
             // End of Problem Two
 
             //Start of Problem Three
-            //Console.WriteLine("This is Problem Three");
-            //ProblemThree problemThree = new ProblemThree();
-            //double returnedAverage = 0;
-            //foreach (var item in problemThree.classGrades)
-            //{
-            //    var average = problemThree.classGrades.Select(g => g.Split(',').Select(int.Parse).ToList().OrderByDescending(i => i).Take(problemThree.classGrades.Count).Average()).Average();
-            //    returnedAverage = Convert.ToDouble(average);
-            //}
-            //Console.WriteLine($"{returnedAverage}");
+            Console.WriteLine();
+            Console.WriteLine("This is Problem Three:");
+            ProblemThree problemThree = new ProblemThree();
+            double returnedAverage = 0;
+            foreach (var item in problemThree.classGrades)
+            {
+                var average = problemThree.classGrades.Select(g => g.Split(',').Select(int.Parse).ToList().OrderByDescending(i => i).Take(problemThree.classGrades.Count).Average()).Average();
+                returnedAverage = Convert.ToDouble(average);
+            }
+            Console.WriteLine($"{returnedAverage}");
             //End of Problem Three
 
             //Start of Problem Four
-            Console.WriteLine("This is Problem Four");
+            Console.WriteLine();
+            Console.WriteLine("This is Problem Four:");
             ProblemFour problemFour = new ProblemFour();
-            //var stringListOrdered = problemFour.inputString.OrderBy(x => x).ToArray().Count();
-            //var stringListOrdered = problemFour.inputString.OrderBy(x => x).ToArray().Count();
-            //Console.WriteLine($"{stringListOrdered}");
-            //List<string> theList = new List<string>();
+            Console.WriteLine($"Initial string: {problemFour.inputString}");
             var returnList = new List<string>();
             var theList = problemFour.inputString.ToUpper().OrderBy(a => a).ToList();
             var stringLetterFrequency = from a in theList group a by a into b select b;
@@ -57,33 +60,9 @@ namespace LINQ_Problems
             {
                 returnList.Add(letterToCount.Key.ToString());
                 returnList.Add(letterToCount.Count().ToString());
-                //Console.Write($"{letterToCount.Key}{letterToCount.Count()}");
             }
-            foreach (var item in returnList)
-            {
-                Console.Write($"{item}");
-            }
-
-            //var stringToList = problemFour.inputString.ToUpper().OrderBy(a => a).ToArray();
-            //var stringLetterFrequency = from a in stringToList group a by a into b select b;
-            //Console.Write("The frequency of the characters are :\n");
-            //var returnString = "";
-            //foreach (var letterToCount in stringLetterFrequency)
-            //{
-            //    Console.Write($"{letterToCount.Key}{letterToCount.Count()}");
-            //}
-            //var count = stringToList.Count(f => f ==(stringToList.Distinct()));
-            //var count = stringToList.TakeWhile(c => c == (stringToList.Distinct()).Count());
-            //var numbersForList = stringToList
-            //foreach (var item in count)
-            //{
-            //    Console.WriteLine($"{item}");
-            //}
-
-            //foreach (var item in stringListOrdered)
-            //{
-            //    Console.WriteLine($"{item}");
-            //}
+            string combinedString = string.Join("", returnList.ToArray());
+            Console.WriteLine($"Return string: {combinedString}");
             //End of Problem Four
             Console.ReadLine();
         }
